cda1b4bfd3fa554a21a334d639cca032e4e760f0
GachauDominic <gachaudominic@gmail.com>
2026-08-27 02:57:09 +0300

    Update package dependencies and integrate CORS middleware for improved API security

--- src/server.ts
+++ src/server.ts
@@ -9,21 +9,25 @@ import reviews from "./reviews/reviews.route"
 import rooms from "./rooms/room.route"
 import express from 'express'
 import type { Express } from 'express'
-
+import cors from 'cors'
 
 const initializeApp = (): Express => {
   // const express = require('express')
   const app = express()
 
+  // {getting data comming in form of a json format}
+  app.use(express.json())
+
   //middlewares
   // {handles url-encoded data ie => form data. By pulling out data as a parameter when it comes in as a URL}
   // app.use(express.urlencoded({extended: false}));
+  app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE',]
+  })) 
 
   app.use(logger)
   app.use(rateLimiterMiddleware)
-
-  // {getting data comming in form of a json format}
-  app.use(express.json())
         
   // routes
   host(app)
@@ -42,6 +46,3 @@ const initializeApp = (): Express => {
 
 const app = initializeApp()
 export default app
-
-
-
